3402f5037b08cfb7a7dea4396ce37475c2c41007
Stefanus Gratilio <121921500+stefanus-gratilio-05@users.noreply.github.com>
2024-08-07 11:27:16 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'belcafe');
+    $conn = mysqli_connect('localhost', 'root', '', 'die gaststatte');
     if(!$conn)
         // Jika koneksi gagal
         echo 'Gagal terhubung ke database: ' . mysqli_connect_error();
